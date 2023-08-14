d03a5564c5176f5833084403098a772cfe875303
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-08-14 20:57:06 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function bulb_On(){
+    const bulbImg= document.getElementById("bulb_img").src="./img/bulb_on.jpg";
+}
+function bulb_Off(){
+    const bulbImg=document.getElementById("bulb_img").src="./img/bulb_off.jpg";
+}
